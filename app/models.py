2e6dbc03f371a6a1c39934aca3128ad622bd65bf
Da <lydia.da.ding@gmail.com>
2020-04-17 15:34:25 -0400

    init db, create dream table

--- app/models.py
+++ app/models.py
@@ -1,34 +1,31 @@
+from datetime import datetime
+
 from app import db
 
 class Dream(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128), index=True)
-    email = db.Column(db.String(64), index=True)
-    name = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(64))
+    name = db.Column(db.String(64))
     location = db.Column(db.String(64), index=True)
-    date = db.Column(db.Date())
+    date = db.Column(db.Date(), index=True)
     content = db.Column(db.Unicode())
-    # submission date
-    submit_date = db.Column(db.DateTime())
-    # accepted
-    accepted = db.Column(db.Boolean(), index=True)
-    published = db.Column(db.Boolean(), index=True)
-    # TODO:
-    # - when to index??
+    timestamp = db.Column(db.DateTime(), index=True, default=datetime.utcnow)
+    is_accepted = db.Column(db.Boolean(), index=True)
 
     def __repr__(self):
-        return f'Dream #{self.id}'
+        return f'<Dream #{self.id}: {self.title}>'
 
 
-class ContentWarning:
+# class ContentWarning:
 
-    def __init__(self, id_, content_warning):
-        self.id_ = id_
-        self.content_warning = content_warning
+#     def __init__(self, id_, content_warning):
+#         self.id_ = id_
+#         self.content_warning = content_warning
 
 
-class Theme:
+# class Theme:
 
-    def __init__(self, id_, theme):
-        self.id_ = id_
-        self.theme = theme
+#     def __init__(self, id_, theme):
+#         self.id_ = id_
+#         self.theme = theme
